d4704220bd5172b2a84d8e764af7cdacb60a91d5
abrilbianchimoraes <abrilbianchi@gmail.com>
2025-11-10 17:49:36 -0300

    Cambio draftosaurussql

--- db/draftosaurus.sql
+++ db/draftosaurus.sql
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `draftosaurus`
 --
-CREATE DATABASE `Draftosaurus`;
-USE DATABASE Draftosaurus;
+
 -- --------------------------------------------------------
 
 --
